471ed74fb83b22306908ce61ef61582aecafc1cd
Suea09 <phiraphat.sukkasem@gmail.com>
2023-07-31 23:05:08 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
 
 @Module({
-  imports: [AuthenticationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...require('../typeorm.config'),
+      entities: [Account],
+    }),
+    AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
